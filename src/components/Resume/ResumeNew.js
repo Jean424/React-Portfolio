14ba52d00e9bc055d9043de1307dec6a6f76d5e3
Jean Lee <jeanlee.jingli@icloud.com>
2022-08-24 20:00:28 -0700

    update files

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/JingLi_CV.pdf";
+import pdf from "../../Assets/Jing(Jean)Li_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/Jean424/Resume/raw/main/JingLi_CV.pdf";
+  "https://github.com/Jean424/Resume/raw/main/Jing(Jean)Li_CV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
